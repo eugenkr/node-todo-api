d3b5015b2d066c4d97185ce04a20672a7f4b146d
Yevgen Kruglyk <eugen.kr@gmail.com>
2017-08-11 16:56:28 +0300

    post to db

---
+++ server/server.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+mongoose.connect('mongodb://localhost:27017/TodoApp');
+
+let Todo = mongoose.model('Todo', {
+  text: {
+    type: String
+  },
+  completed: {
+    type: Boolean
+  },
+  completedAt: {
+    type: Number
+  }
+});
+
+let newTodo = new Todo({
+  text: 'Buy iWatch',
+  completed: false
+});
+
+newTodo.save().then(
+  (doc) => {
+    console.log('Saved todo', doc);    
+  },
+  (error) => {
+    console.log('Unable to save todo')
+  }
+);
