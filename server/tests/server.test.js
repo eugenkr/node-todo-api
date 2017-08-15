26750ed19e8faf2ec522c5476d132976b2d838e3
Yevgen Kruglyk <eugen.kr@gmail.com>
2017-08-15 14:19:51 +0300

    test DELETE /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -99,5 +99,46 @@ describe('GET /todos/:id', () => {
     .get(`/todos/123`)
     .expect(404)
     .end(done);
+  });
+});
+
+describe('DELETE /todos/:id', () => {
+  it('should remove a todo', done => {
+    let id = todos[1]._id.toHexString();
+
+    request(app)
+      .delete(`/todos/${ id }`)
+      .expect(200)
+      .expect(res => {
+        expect(res.body.todo._id).toBe(id);
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+
+        Todo.findById(id)
+          .then(todo => {
+            expect(todo).toNotExist();
+            done();
+          })
+          .catch(e => done(e));
+      });
+  });
+
+  it('return 404 if todo not found', done => {
+    let id = new ObjectID().toHexString();
+    
+    request(app)
+      .delete(`/todos/${ id }`)
+      .expect(404)
+      .end(done);
+  });
+
+  it('return 404 if object id is invalid', done => {
+    request(app)
+    .delete(`/todos/123`)
+    .expect(404)
+    .end(done);
 });
 });
